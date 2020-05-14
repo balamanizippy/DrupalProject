b7b5db8abb8b21f536a3d9717026d043e6a5afe3
1996karthick <61176771+1996karthick@users.noreply.github.com>
2020-05-14 22:02:12 +0530

    Update variables.tf

--- DrupalwithAutoscalingLoadBalancer/variables.tf
+++ DrupalwithAutoscalingLoadBalancer/variables.tf
@@ -7,10 +7,10 @@ variable "security_key" {
 variable "aws_region" {
     default = "us-east-1"
 }
-variable "image" {
-    description = "instance images"
-    default     = "ami-0323c3dd2da7fb37d"
-}
+#variable "image" {
+#    description = "instance images"
+#    default     = "ami-0323c3dd2da7fb37d"
+#}
 variable "instance_type" {
     description = "instance type"
     default     = "t2.micro"
