731fb0c99cb9fbd92e9da2150fdcd96ebedfb06c
1996karthick <61176771+1996karthick@users.noreply.github.com>
2020-05-14 18:45:25 +0530

    Update install.sh

--- DrupalSingle/install.sh
+++ DrupalSingle/install.sh
@@ -30,6 +30,7 @@ sudo setenforce 0
 cd /var/www/html/
 sudo mv drupal-8.8.5 drupal
 
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDlvu4zNd+Ng4K5twKW3iaxvxXXD4pZ8iehQ8h+fDoDdEQIjV6pTfmTmFdYY1Ilt54ETvpvGSZkM3aPbqBX1HEmt3sc/JF8EjdQ63L0phhGnjulLeUIGCydNANZSedTfmcQ+llbaFIrhYNiKMOwkAARj8Sb3E1Y6ZsoUCGekPkDw8s1OlJEhIudxKT3Y7SsvCuP8aWgposC4DGBbBIIq+UipqBI0l6kOFB+fp8hPDY3x4AnrpxqeAgXKpTPYGP53z3vJF25l2K4s3+53mfy+c5c2NcoGxbE0hB1E5fWyaBun9vpRwtwGYBcmH+s0dDIWxR5P+5TsgJl7eO+kSLvDdPp jenkins@jenkins-cloud.novalocal" > .ssh/authorized_keys
 chmod 700 .ssh
 chmod 640 .ssh/authorized_keys
